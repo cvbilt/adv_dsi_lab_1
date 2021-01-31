8b3227f3c252099c1cf635a01b055bcd5eb7a1f2
cvbilt <tampilprimananda@Tampils-MacBook-Pro.local>
2021-01-31 16:43:53 +1100

    ADSI Lab 1 Exercise 2

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='adv dsi lab 1 exercise 2',
+    author='Charlie',
+    license='MIT',
+)
